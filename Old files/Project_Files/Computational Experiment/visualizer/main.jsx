8b56d05365ce5589c46ff2908534c3a23f51a965
tommorbia333 <tommorbia@gmail.com>
2026-06-24 13:51:52 +0100

    Stop tracking Old files folder

--- Old files/Project_Files/Computational Experiment/visualizer/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import EventGraphVisualizer from './EventGraphVisualizer.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <EventGraphVisualizer />
-  </React.StrictMode>
-)
--- Old files/Project_Files/README.md
+++
@@ -1,2 +0,0 @@
-# Dissertation
-Experiment question: how does temporal structure in narratives impact human and machine causal understanding and reasoning?
